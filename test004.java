c945449e14bbd6c0a5c559f0124e7ae3ca4e3beb
yunstory <73205023+yunstory@users.noreply.github.com>
2020-10-22 00:23:53 +0900

    Add files via upload

--- test004.java
+++ test004.java
@@ -8,10 +8,19 @@ public class test004 {
 		//1번
 //		int a = 2;
 //		int b = 1;
-//		b ++ ;
+//		b++;
 //		
+//		while(b<15) {
+//			a += 1;
+//			b += a;
+//		}
 //		System.out.println("a 값 : " + a + " b값 : " + b);
-		
+//		
+//
+//		for(b++; b<15; b+=a) {
+//			
+//		}
+//		System.out.println("a 값 : " + a + " b값 : " + b);
 		
 		//2번 
 
@@ -69,9 +78,10 @@ public class test004 {
 //		
 //		for(int i=a; i<=100; i++) {
 //			sum += i;
-//			System.out.println(sum);
 //			if(sum>100)
 //				break;
+//			System.out.println(sum);
+//			
 //		}
 		
 		
@@ -123,37 +133,178 @@ public class test004 {
 		
 		
 		//17번
-		System.out.print("행입력");
-		int a = Integer.parseInt(sc.nextLine());
-		System.out.print("열입력");
-		int b = Integer.parseInt(sc.nextLine());
+//		System.out.print("행입력");
+//		int a = Integer.parseInt(sc.nextLine());
+//		System.out.print("열입력");
+//		int b = Integer.parseInt(sc.nextLine());
+//		
+//		int i,j,k=1;
+//		
+//		for( i=1; i<=a; i++) {
+//			for( j=1; j<=b; j++) {
+//				System.out.print(k+"\t");
+//				k++;
+//				if(k>100) {
+//					break;
+//				}
+//			}
+//			System.out.println();
+//		}
 		
-		int i,j,k;
-		
-		for( i=1; i<=a; i++) {
-			for( j=1; j<=b; j++) {
-				for( k=1; k<=b; k++) {
-					if(k==100) {
-						break;
-					}
-					System.out.print(k);
-				}
-				System.out.println();
-			}
-		}
+
 		//18번
+		
+//		int i,j,k=1;
+//		for(i=1; i<=4; i++) {
+//			if(i==1) {
+//				for(j=1; j<=5; j++) {
+//				System.out.print(k+"\t");
+//				k++;
+//				}
+//			}else if(i==2) {
+//				k=10;
+//				for(j=1; j<=5; j++) {
+//					System.out.print(k+"\t");
+//					k--;
+//				}
+//			}else if(i==3) {
+//				k=11;
+//				for(j=1; j<=5; j++) {
+//					System.out.print(k+"\t");
+//					k++;
+//					}
+//			}else if(i==4) {
+//				k=21;
+//				for(j=1; j<=5; j++) {
+//					System.out.print(k+"\t");
+//					k++;
+//				}
+//			}
+//			System.out.println();
+//		}
+		
 		
 		//19번
 		
-		//20번
+//		System.out.print("첫 요일 입력 (일~토) : ");
+//		String days=sc.nextLine();
+//		System.out.print("마지막 일 입력 (~31): ");
+//		int last=Integer.parseInt(sc.nextLine());
+//		int day;
+//		
+//		
+//		System.out.println();
+//		System.out.print("일\t월\t화\t수\t목\t금\t토");
+//		System.out.println();
+//		
+//		switch(days) {
+//		case "일":
+//			for(day=1; day<=last; day++) {
+//			System.out.print(day+"\t");
+//			if(day%7==0)
+//				System.out.println();
+//			}
+//			break;
+//		case "월":
+//			System.out.print("\t");
+//			for(day=1; day<=last; day++) {
+//			System.out.print(day+"\t");
+//			if((day+1)%7==0)
+//				System.out.println();
+//			}
+//			break;
+//		case "화":
+//			System.out.print("\t\t");
+//			for(day=1; day<=last; day++) {
+//			System.out.print(day+"\t");
+//			if((day+2)%7==0) 
+//				System.out.println();
+//			}
+//			break;
+//		case "수":
+//			System.out.print("\t\t\t");
+//			for(day=1; day<=last; day++) {
+//			System.out.print(day+"\t");
+//			if((day+3)%7==0) 
+//				System.out.println();
+//			}
+//			break;
+//		case "목":
+//			System.out.print("\t\t\t\t");
+//			for(day=1; day<=last; day++) {
+//			System.out.print(day+"\t");
+//			if((day+4)%7==0) 
+//				System.out.println();
+//			}
+//			break;
+//		case "금":
+//			System.out.print("\t\t\t\t\t");
+//			for(day=1; day<=last; day++) {
+//			System.out.print(day+"\t");
+//			if((day+5%7)==0)
+//				System.out.println();
+//			}
+//			break;
+//		case "토":
+//			System.out.print("\t\t\t\t\t\t");
+//			for(day=1; day<=last; day++) {
+//			System.out.print(day+"\t");
+//			if((day+6)%7==0)
+//				System.out.println();
+//			}
+//			break;
+//		default:
+//			System.out.print("입력 오류");
+//		}
 		
+		//20번 
+//		int right=0;
+//		int wrong=0;
+//
+//		while(true) {
+//		System.out.print("선택(0.앞면 1.뒷면 3.종료) : ");
+//		int or=Integer.parseInt(sc.nextLine());
+//		int coin = (int)(Math.random()*2);
+//		if(or==coin) {
+//			right++;
+//		}else {
+//			wrong++;
+//		}
+//		if(or==3) {
+//		System.out.print("총"+(right+wrong)+"번의 게임 중  맞춘횟수:"+right+"틀린횟수:"+wrong);
+//			break;
+//		}
+//		System.out.println("결과 / 내 선택:"+or+" 컴퓨터 값:"+coin+" 맞춘횟수 : "+right+", 틀린횟수 : "+wrong+" /총 게임 실행 횟수:"+(right+wrong)+"\n");
+//		}
+
 		//21번
 		
-		
-		
-		
-		
-		
+//		int right=0;
+//		int wrong=0;
+//		
+//		
+//		
+//
+//		while(true) {
+//			System.out.print("주사위 값 입력 / 7.종료 : ");
+//			int die=Integer.parseInt(sc.nextLine());
+//			
+//			int dice = (int)(Math.random()*6)+1;
+//			
+//			if(die==dice) {
+//				right++;
+//			}else {
+//				wrong++;
+//			}
+//			if(die==7) {
+//				System.out.print("총"+(right+wrong)+"번의 게임 중 맞춘횟수 : "+right+"  틀린횟수 : "+wrong);
+//				break;
+//			}
+//			System.out.println("결과 / 내 주사위:"+die+" 컴퓨터 주사위:"+dice+" 맞춘횟수 : "+right+", 틀린횟수 : "+wrong+" /총 게임 실행 횟수:"+(right+wrong)+"\n");
+//			
+//			
+//		}
+	
 		
 	}
 
